dde668e90c2c4be59c94b63b7f7c81904ac8f019
jefflai108 <jefflai108@gmail.com>
2022-02-15 13:20:12 -0500

    update

--- scripts/viz.sh
+++ scripts/viz.sh
@@ -10,14 +10,10 @@ feature=hubert6; feature_dim=768
 feature=hubert8; feature_dim=768
 feature=hubert10; feature_dim=768
 feature=$4
+ckpt=$5
 datadir=data/SpokenCOCO
 expdir=exp/spokencoco/force_aligned_whole_${feature}_embed${embed_size}_lr${lr}_${basename}
-i=0
-while [ $i -ne 20 ]; do  
-    if [ -f ${expdir}/${i}.pth.tar ]; then
-        #echo evaluating ${i}.pth.tar
-        python src/test.py --data_path ${datadir}/Freda-formatting/ --candidate ${expdir}/${i}.pth.tar --vocab_path ${datadir}/SpokenCOCO_vocab-threshold1.pkl --basename ${basename}
-    fi 
-    i=$(($i+1))
-done
-exit 0
+if [ -f ${expdir}/${ckpt}.pth.tar ]; then
+    echo evaluating ${ckpt}.pth.tar
+    python src/test.py --data_path ${datadir}/Freda-formatting/ --candidate ${expdir}/${ckpt}.pth.tar --vocab_path ${datadir}/SpokenCOCO_vocab-threshold1.pkl --basename ${basename} --visual_tree
+fi 
