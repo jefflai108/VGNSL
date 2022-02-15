87ca828965f9cc2834aac453446e47387b92b3ab
jefflai108 <jefflai108@gmail.com>
2022-02-15 14:18:49 -0500

    update

--- scripts/training/force_aligned_whole_hubert+km.sh
+++ scripts/training/force_aligned_whole_hubert+km.sh
@@ -14,7 +14,7 @@ feature=content_vec_v12_0512; feature_dim=768
 feature=$4
 datadir=data/SpokenCOCO
 km_cluster=$5
-stage=100
+stage=1
 
 if [ $stage -eq 1 ]; then 
 expdir=exp/spokencoco/force_aligned_whole_${feature}_km${km_cluster}-discrete_word-embed${embed_size}_lr${lr}_${basename}
@@ -37,16 +37,3 @@ python src/train.py --logger_name $expdir \
     --speech_hdf5 --feature ${feature} --load_pretrained \
     --discretized_phone --km_clusters $km_cluster
 fi 
-
-if [ $stage -eq 100 ]; then 
-expdir=exp/spokencoco/force_aligned_whole_${feature}_km${km_cluster}-discrete_word-embed${embed_size}_lr${lr}_${basename}
-python src/train.py --logger_name $expdir \
-    --data_path ${datadir}/Freda-formatting --vocab_path ${datadir}/SpokenCOCO_vocab.pkl --basename ${basename} \
-    --init_embeddings 0 --img_dim 2048 --scoring_hidden_dim 128 \
-    --num_epochs 20 --workers 5 --batch_size 5 --margin 0.2 \
-    --embed_size ${embed_size} --feature_dim ${feature_dim} --learning_rate ${lr} \
-    --speech_hdf5 --feature ${feature} --load_pretrained \
-    --discretized_word --km_clusters $km_cluster
-fi 
-
-
