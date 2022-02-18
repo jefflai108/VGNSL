e820d9dac584469cbeae461d25499679abf6c14e
jefflai108 <jefflai108@gmail.com>
2022-02-18 17:33:03 -0500

    add tree viz

--- scripts/viz.sh
+++ scripts/viz.sh
@@ -10,17 +10,15 @@ feature=hubert6; feature_dim=768
 feature=hubert8; feature_dim=768
 feature=hubert10; feature_dim=768
 feature=$4
-ckpt=$5
+last_ckpt=$5
 
-for ckpt in `seq -s ' ' 0 9`; do 
-
-datadir=data/SpokenCOCO
-expname=force_aligned_whole_${feature}_embed${embed_size}_lr${lr}_${basename}
-expdir=exp/spokencoco/${expname}
-vis_fpath=tree_viz_for_Freda/${expname}_ckpt${ckpt}.txt
-if [ -f ${expdir}/${ckpt}.pth.tar ]; then
-    echo evaluating ${ckpt}.pth.tar
-    python src/test.py --data_path ${datadir}/Freda-formatting/ --candidate ${expdir}/${ckpt}.pth.tar --vocab_path ${datadir}/SpokenCOCO_vocab-threshold1.pkl --basename ${basename} --visual_tree > $vis_fpath
-fi 
-
+for ckpt in `seq -s ' ' 0 ${last_ckpt}`; do
+    datadir=data/SpokenCOCO
+    expname=force_aligned_whole_${feature}_embed${embed_size}_lr${lr}_${basename}
+    expdir=exp/spokencoco/${expname}
+    vis_fpath=tree_viz_for_Freda/${expname}_ckpt${ckpt}.txt
+    if [ -f ${expdir}/${ckpt}.pth.tar ]; then
+        echo evaluating ${ckpt}.pth.tar
+        python src/test.py --data_path ${datadir}/Freda-formatting/ --candidate ${expdir}/${ckpt}.pth.tar --vocab_path ${datadir}/SpokenCOCO_vocab-threshold1.pkl --basename ${basename} --visual_tree > $vis_fpath
+    fi 
 done 
