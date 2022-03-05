02e0f3db80361082a768ea5e91c995fb9dcebc80
jefflai108 <jefflai108@gmail.com>
2022-03-05 10:46:44 -0500

    update

--- scripts/training/phn_force_aligned_whole_hubert.sh
+++ scripts/training/phn_force_aligned_whole_hubert.sh
@@ -21,6 +21,6 @@ expdir=exp/spokencoco/phn_force_aligned_whole_${feature}_embed${embed_size}_lr${
 python src/train.py --logger_name $expdir \
     --data_path ${datadir}/Freda-formatting --vocab_path ${datadir}/SpokenCOCO_phn_vocab-threshold1.pkl --basename ${basename} \
     --init_embeddings 0 --img_dim 2048 --scoring_hidden_dim 128 \
-    --num_epochs 10 --workers 0 --batch_size 64 --margin ${margin} \
+    --num_epochs 10 --workers 0 --batch_size 64 --margin ${margin} --val_step 1500 \
     --embed_size ${embed_size} --feature_dim ${feature_dim} --learning_rate ${lr} --lambda_hi ${head_init_bias} \
     --speech_hdf5 --feature ${feature} --load_pretrained --phn_force_align
