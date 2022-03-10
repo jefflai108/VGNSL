03592401500eb6bad8870d647ae0d0e81831fe35
jefflai108 <jefflai108@gmail.com>
2022-03-10 01:51:24 -0500

    update

--- src/differential_boundary.py
+++ src/differential_boundary.py
@@ -171,42 +171,6 @@ class DifferentialWordSegmentation(nn.Module):
         
         return word_segment_rep
 
-class WordEncoderMask(nn.Module):
-    """
-    Word encoder considering masking
-    """
-    def __init__(self, hidden_dim):
-        super(AttentivePooling, self).__init__()
-        
-        self.feature_transform = nn.Linear(hidden_dim, hidden_dim)
-        self.W_a = nn.Linear(output_dim, output_dim)
-        self.W = nn.Linear(output_dim, 1)
-        self.act_fn = nn.ReLU()
-        self.softmax = nn.functional.softmax
-
-    def forward(self, batch_rep, att_mask):
-        """
-        input:
-        batch_rep : size (B, T, H), B: batch size, T: sequence length, H: Hidden dimension
-        att_mask:  size (B, T),     Attention Mask logits
-        
-        attention_weight:
-        att_w : size (B, T, 1)
-        
-        return:
-        utter_rep: size (B, H)
-        """
-        batch_rep  = self.feature_transform(batch_rep)
-        att_logits = self.W(self.act_fn(self.W_a(batch_rep))).squeeze(-1)
-        att_logits = att_mask + att_logits # masked out frames recieves ~0% prob. 
-        # compute attention att_w
-        # softmax over segment dimension i.e. take the most representation frame to represent word 
-        att_w = self.softmax(att_logits, dim=-1).unsqueeze(-1)
-        # apply att_w to input
-        segment_rep = torch.sum(batch_rep * att_w, dim=1) 
-
-        return segment_rep
-
 if __name__ == '__main__': 
     batch_size = 10
     num_word = 20
