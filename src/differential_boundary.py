6b91387eeac5edad11b8c5ba62f4fa98ece6d3d0
jefflai108 <jefflai108@gmail.com>
2022-03-10 21:35:46 -0500

    improve diff boundary

--- src/differential_boundary.py
+++ src/differential_boundary.py
@@ -1,6 +1,7 @@
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np 
 
 from module import AttentivePooling
 from utils import l2norm
@@ -91,9 +92,36 @@ def segment_peak_detector(segment_dissimilarity, threshold):
     )
     return peak_detector 
 
+def top_k_peak_sampling(P, phn_mask, gt_word_lens, proxy_large_num=10):
+    """
+    top_k selection on peak to match # of words specified in phn_mask
 
+    input: 
+    P: size (B, N), B: batch size, N: number of phns
+    phn_mask : size (B, N)
+    gt_word_lens: size (B,)
+
+    output: 
+    P: size (B, N)
+    """
+    # iterate through P as each Pi has different # of gt words 
+    # ensure top-k word segments are selected from *the valid phn ranges*
+    for i in range(len(P)): 
+        num_of_words = gt_word_lens[i]
+        num_of_phns = torch.sum(torch.where(phn_mask[i] != -100000, 1, 0)).item()
+        P[i, 0] = proxy_large_num # utt has to start with segmentation. Make it large so that first seg must be selected by top-k
+        topk_Pi, topk_Pi_idx = torch.topk(P[i, :num_of_phns], num_of_words) # for the corner case of under-segmentation. trivial solve by randomly uniform segment the rest (return from topk sampling)
+        P[i, topk_Pi_idx] = proxy_large_num
+    
+    P = torch.where(P != proxy_large_num, torch.tensor(0.0), P) # rest of P set to 0. This will avoid over-segmentation.
+
+    # good way to debug
+    #print(P)
+    #print(gt_word_lens)
+    return P
+    
 class DifferentialWordSegmentation(nn.Module): 
-    def __init__(self, hidden_dim, peak_detection_threshold, **kwargs):
+    def __init__(self, hidden_dim, peak_detection_threshold):
         super(DifferentialWordSegmentation, self).__init__()
         
         self.adjacent_segment_sim_measure = SegmentSimilarityMeasure(hidden_dim)
@@ -104,14 +132,15 @@ class DifferentialWordSegmentation(nn.Module):
             nn.Linear(hidden_dim, hidden_dim)
         )
 
-    def forward(self, segment_rep, phn_mask):
+    def forward(self, segment_rep, phn_mask, gt_word_lens):
         '''
         input:
         segment_rep : size (B, N, H), B: batch size, N: number of phns, H: Hidden dimension
         phn_mask : size (B, N)
-        
+        gt_word_lens: size (B,)
+
         return:
-        word_segment_rep: size (B, H)
+        word_segment_rep: size (B, M, H), M: max number of words in the minibatch
         '''
         batch_size, num_phns, hidden_dim = segment_rep.shape
         
@@ -124,66 +153,86 @@ class DifferentialWordSegmentation(nn.Module):
     
         # 2. Run first/second-order peak detector on D 
         P = segment_peak_detector(D, self.peak_detection_threshold)
-        P = F.pad(P, (0, 1), value=0) # for matching input_shape. Since there's no next frame for last, Pt should be 0. 
-        print(P.shape) # B, N 
+        P = F.pad(P, (0, 1), value=0) # for matching input_shape. Since there's no next frame for last, Pt should be 0.
+        print(P.shape) # B, N
+
         # 2.1 filter out padded phn from P accroding to phn_mask
         P = P + torch.sub(phn_mask, torch.ones_like(phn_mask))
         F.relu(P, inplace=True)
-        #print(P)
+        print(P)
+
+        # 2.2 top-k sampling for enforcing # of word segment
+        P = top_k_peak_sampling(P, phn_mask, gt_word_lens)
 
         # 3. make P binary and differentiable via straight-through
         b_soft = torch.tanh(10 * P)
         b_hard = torch.tanh(100000 * P)
         b = b_soft + (b_hard - b_soft).detach()
         print(b.shape) # B, N 
+
+        # these two are good way to debug 
         #print(b)
+        #print(gt_word_lens)
 
         # 4. vectorize b for mean-pooling 
         b = torch.cumsum(b, dim=1) 
-        for i in range(batch_size): 
-            if b[i][0] == 0: # detect and fix first segments
-                b[i] = torch.add(b[i], 1)
-        num_word_segments = b[:, -1].int() + 1
+        #print(b)
+        assert b[:, -1].tolist() == gt_word_lens, print('num of word segments should equal the number of words')
+        num_word_segments = b[:, -1].int()
         U = -100000 * torch.ones(batch_size, num_phns, max(num_word_segments))
         for i in range(batch_size): 
             num_word_segment = num_word_segments[i]
             U[i, :, :num_word_segment] = torch.arange(1, num_word_segment+1).repeat(num_phns, 1)
-        print(U.shape) # B, N, # of segments
+        print(U.shape) # B, N, M 
         V = U - b.unsqueeze(-1)
         #print(V)
-        print(V.shape) # B, N, # of segments 
+        print(V.shape) # B, N, M
         W = 1 - torch.tanh(100000 * torch.abs(V))
         W = W / torch.sum(W, dim=1).unsqueeze(1)
         W = torch.nan_to_num(W)
-
         # these two are good way to debug 
-        #print(b[-1])
-        #print(W[-1])
-        print(W.shape) # B, N, # of segments 
+        #print(b[-2])
+        #print(W[-2])
+        #print(gt_word_lens)
+        #print(W.shape) # B, N, M
 
         # 5. word segment representation via W
-        W = W.permute(0, 2, 1) # B, # of segments, N 
+        W = W.permute(0, 2, 1) # B, M, N 
         word_segment_rep = torch.bmm(W, segment_rep)
-        print(word_segment_rep.shape)
+        print(word_segment_rep.shape) # B, M, H
         #print(word_segment_rep[-1])
         word_masks = torch.where(word_segment_rep==0, 0, 1)
         #print(word_masks[-1])
         word_segment_rep = self.word_enc(word_segment_rep)
         word_segment_rep = torch.mul(word_segment_rep, word_masks)
-        
+        print(word_segment_rep.shape) # B, M, H
+        #print(word_segment_rep[-1, 5:])
         return word_segment_rep
 
+'''
+some to-dos:
+
+1. use scipy.peak-detector during test-time 
+2. enforce # of words via selecting top-k peaks (instead of thresholding) --> done 
+3. Remove <sos> <eos> for word segmentation? Altentatively, # detect and fix "first and second" segments instead, to make <sos> itself a segment. Perhaps also need to do it for <eos> too. Make <eos> itself a segment
+    i.e. the real segments are n-2 --> remove <sos> <eos> during pre-processing instead. 
+
+some potential improvements: 
+1. run an RNN on phn-segments (like SCPC). This way we can have both the loss, and word similarity comes from 
+    cos_dist(context_vector, segment_vector). 
+'''
 if __name__ == '__main__': 
     batch_size = 10
     num_word = 20
     frame_per_segment = 15
     feat_dim = 768
     hidden_dim = 512
-    word_segment_threshold = 0.03
+    word_segment_threshold = 0.00 # for enforcing # of words 
+    gt_word_lens = [2, 3, 5, 2, 6, 3, 4, 9, 7, 4]
     
     x = torch.randn(batch_size * num_word, frame_per_segment, feat_dim)
     audio_mask = torch.ones(batch_size, num_word, frame_per_segment)
-    audio_mask[:, -10:, :] = -100000 # half of the phns are masked out 
+    audio_mask[:-1, -10:, :] = -100000 # half of the phns are masked out 
     audio_mask_reshaped = audio_mask.reshape(-1, frame_per_segment)
 
     sem_embedding = AttentivePooling(feat_dim, hidden_dim)
@@ -193,5 +242,5 @@ if __name__ == '__main__':
     print(sem_embeddings.shape) # torch.Size([10, 48, 512])
      
     differential_boundary_module = DifferentialWordSegmentation(hidden_dim, word_segment_threshold)
-    differential_boundary_module(sem_embeddings, audio_mask[:, :, 0])
+    differential_boundary_module(sem_embeddings, audio_mask[:, :, 0], gt_word_lens)
 
