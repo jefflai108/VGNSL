612a03456b4aaa949ad3a9c93131731587e1bfa3
jefflai108 <jefflai108@gmail.com>
2022-05-03 13:11:10 -0400

    update

--- self-attentive-parser/scripts/testing/text/wsj.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python src/main.py test \
-    --test-path "data/23.auto.clean" \
-    --no-predict-tags \
-    --model-path models/English_charlstm_dev*.pt
--- self-attentive-parser/scripts/training/text/wsj.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python src/main.py train \
-    --train-path "data/02-21.10way.clean" \
-    --dev-path "data/22.auto.clean" \
-    --use-chars-lstm --use-encoder --num-layers 8 \
-    --batch-size 250 --learning-rate 0.0008 \
-    --model-path-base models/English_charlstm
